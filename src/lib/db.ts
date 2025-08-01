8d6a67673638b508d85e755a9c02900ea4769bdb
zainAwan9175 <zain9175zain@gmail.com>
2025-08-01 23:48:34 +0500

    mongo db key error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI:any = process.env.MONGODB_URI 
+// const MONGODB_URI:any = process.env.MONGODB_URI 
 
 export const connectDB = async () => {
   try {
     if (mongoose.connections[0].readyState) {
       return
     }
-    await mongoose.connect(MONGODB_URI)
+    await mongoose.connect("mongodb+srv://root:root@cluster0.gut2gfz.mongodb.net/portfolioDB?retryWrites=true&w=majority&appName=Cluster0")
     console.log("MongoDB connected successfully")
   } catch (error) {
     console.error("MongoDB connection error:", error)

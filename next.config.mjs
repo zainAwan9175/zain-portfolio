9672019f19de3e29a9f49cf3367ca35032850818
Zain <zain9175zain@gmail.com>
2026-06-24 20:22:01 +0500

    chore: project setup, config, and dependencies

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+    unoptimized: true,
+  },
+}
+
+export default nextConfig;
